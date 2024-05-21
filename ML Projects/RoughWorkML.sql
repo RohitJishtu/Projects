a04408c590a3955ff06a596d26ccab60bde1e83d
Rohit Jishtu <jishtu.rohit@gmail.com>
2024-05-21 11:19:34 +0530

    Today's commit

---
+++ ML Projects/RoughWorkML.sql
@@ -0,0 +1,18 @@
+Named Entity Recognition (NER)
+Latent Dirichlet Allocation (LDA)
+
+
+
+Answer Key:
+b) A way to encode words as vectors in a high-dimensional space
+b) 
+b) A metric for evaluating machine translation
+b) Language modeling
+b) They can handle long-term dependencies better than traditional RNNs
+b) A context-aware embedding model that processes words in both directions
+c) Precision, Recall, and F1-score
+b) To focus on different parts of the input sequence with varying levels of importance
+a) Pre-training a model on a large dataset and then adjusting it with a smaller, task-specific dataset
+b) Identifying the correct meaning of a word based on context
+
+
